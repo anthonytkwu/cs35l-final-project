1cf9f078f2cd977e90ecc5e41b670f72b3e8d79a
anthonytkwu <anthonytkwu@gmail.com>
2024-05-14 16:07:05 -0700

    table info diagnostics

--- db_testing.py
+++ db_testing.py
@@ -1,9 +1,24 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
+connection = mysql.connector.connect(
   host="database-1.c94cq6qeuzwb.us-west-1.rds.amazonaws.com",
   user="cs35l_user",
   password="Anthony57085Giants"
 )
+query = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'db1';"
+cursor = connection.cursor()
+# Execute the query
+cursor.execute(query)
 
-print(mydb)
+# Fetch all rows (table names)
+tables = cursor.fetchall()
+
+# Print out the table names
+print("Tables in the database:")
+for table in tables:
+    print(table[0])
+
+# Close cursor and connection
+cursor.close()
+connection.close()
+
