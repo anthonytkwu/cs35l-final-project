a49e5b116c697aad23428a56267a2074d2c4db75
anthonytkwu <anthonytkwu@gmail.com>
2024-05-14 16:10:45 -0700

    updated passwords and more diagnostics

--- db_testing.py
+++ db_testing.py
@@ -3,7 +3,7 @@ import mysql.connector
 connection = mysql.connector.connect(
   host="database-1.c94cq6qeuzwb.us-west-1.rds.amazonaws.com",
   user="cs35l_user",
-  password="Anthony57085Giants"
+  password="pHPFsXESVGgq9MrP20XY"
 )
 # Create a cursor object to execute SQL queries
 cursor = connection.cursor()
