d80e1fa9fd9fd777170fbba7eb87e76a9c8e151c
Daniel Hughes <daniel.p04.hughes@gmail.com>
2024-05-15 15:26:03 -0700

    message update

--- basic-form-uploader/src/App.js
+++ basic-form-uploader/src/App.js
@@ -1,27 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 function App() {
   const [ name, setName ] = useState('');
   const [ gameID, setGameID ] = useState('');
+  const [ file, setFile ] = useState(null);
+  const [ showForm, setShowForm ] = useState(true);
+  
+  let output = "output";
+  function handleSubmit(event) {
+    setName(event.target.elements.name.value);
+    setGameID(event.target.elements,gameID.value);
+    setShowForm(false);
+    output = 'Name: ' + name + 'Game ID: ' + gameID;
+  }
+
+  function handleFileChange(e) {
+    setFile(URL.createObjectURL(e.target.files[0]));
+  }
 
   return (
-    <form>
+    <>
       <div>
-        <label>
-          Name/Id:
-          <input type="text" name="name" />
-        </label>
-      </div>
-      <div>
-       <label>
-         Game ID:
-         <input type="text" name="gameID" />
-        </label>
-      </div>
-      <button type="submit" value="Submit">Submit</button>
-    </form>
+      {showForm && (
+        <form onSubmit={handleSubmit}>
+         <div>
+            <label>
+             Name/Id:
+             <input type="text" name="name" />
+           </label>
+         </div>
+         <div>
+          <label>
+            Game ID:
+            <input type="text" name="gameID" />
+           </label>
+         </div>
+         <div>
+          <label>
+            Add Image:
+            <input type="file" onChange={handleFileChange}/>
+            <img src={file}/>
+          </label>
+         </div>
+         <button type="submit" value="Submit">Submit</button>
+        </form>
+     )}
+     </div>
+     <div className="output">{output}</div>
+    </>
   );
 }
 
