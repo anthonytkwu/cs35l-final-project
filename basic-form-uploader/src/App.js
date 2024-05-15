01d6769d2bd2af54a12e4be7de634a96bd4aac6d
Daniel Hughes <daniel.p04.hughes@gmail.com>
2024-05-15 00:51:43 -0700

    im and idiot

--- basic-form-uploader/src/App.js
+++ basic-form-uploader/src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [ name, setName ] = useState('');
+  const [ gameID, setGameID ] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <div>
+        <label>
+          Name/Id:
+          <input type="text" name="name" />
+        </label>
+      </div>
+      <div>
+       <label>
+         Game ID:
+         <input type="text" name="gameID" />
+        </label>
+      </div>
+      <button type="submit" value="Submit">Submit</button>
+    </form>
   );
 }
 
