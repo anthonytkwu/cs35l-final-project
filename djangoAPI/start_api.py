c35b10d2ef36327f0a6762b5292ebe6ead374d4a
Jacob Freund <jacobfreund01@gmail.com>
2024-06-02 00:47:43 +0000

    added startup script for api that processes in the background

--- djangoAPI/start_api.py
+++ djangoAPI/start_api.py
@@ -7,5 +7,4 @@ from djangoAPI.wsgi import application
 
 if __name__ == '__main__':
     from waitress import serve
-    print("Application running at http://13.52.151.206:80/api")
     serve(application, host='0.0.0.0', port=80)
