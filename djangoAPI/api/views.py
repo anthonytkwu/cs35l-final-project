f156ed2f2e7c311f7d2f1fa402e49800d26a0f7b
anthonytkwu <anthonytkwu@gmail.com>
2024-06-03 13:13:53 -0700

    init for testing

--- djangoAPI/api/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from .models import Image
-from .serializers import ImageSerializer
-
-class ImageViewSet(viewsets.ModelViewSet):
-    queryset = Image.objects.all()
-    serializer_class = ImageSerializer
