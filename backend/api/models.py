ebc907379f35594818c14976625306d8bcb3d677
anthonytkwu <anthonytkwu@gmail.com>
2024-06-04 17:28:13 -0700

    added functionality for having client send images to server. changed .gitignore so that migrations, .vscode files are not tracked.

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,18 +3,16 @@ from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.core.files.base import ContentFile
 from random import randint, shuffle
+import os
 
 # Create your models here.
-    
-    
-
 class Session(models.Model):
     game_code = models.IntegerField(unique=True, default=0, validators=[MinValueValidator(0), MaxValueValidator(999999)], primary_key=True)
     draw_time = models.IntegerField(choices=[(30, '30 seconds'), (45, '45 seconds'), (60, '60 seconds')])
     desc_time = models.IntegerField(choices=[(15, '15 seconds'), (30, '30 seconds'), (45, '45 seconds')])
     created_at = models.DateTimeField(auto_now_add=True)
     users = models.ManyToManyField(User, related_name='sessions')
-    round = models.IntegerField(default=0, validators=[MinValueValidator(-1), MaxValueValidator(10)])
+    round = models.IntegerField(default=-1, validators=[MinValueValidator(-2), MaxValueValidator(10)])
     last_modified = models.DateTimeField(auto_now=True)
     # round: 0 is lobby, 1-10 is drawing/word guessing, -1 is game over
 
@@ -48,7 +46,7 @@ class Session(models.Model):
             
             return result
         
-        self.round = 1
+        self.round = 0
         self.save()
 
         chains = generate_chains(list(self.users.all()))
@@ -78,25 +76,32 @@ class Description(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='descriptions')
 
     def __str__(self):
-        return self.id
+        return str(self.id)
 
 class Drawing(models.Model):
+    id = models.AutoField(primary_key=True)
     drawing = models.FileField()
     chain = models.ForeignKey(Chain, on_delete=models.CASCADE)
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='drawings')
 
     def save(self, *args, **kwargs):
         # Save the instance to the database to get an id
-        self.drawing = None
-        super().save(*args, **kwargs)
 
         # Save the file with the correct name
         file_content = self.drawing.file.read()
-        self.drawing.save(f'{self.id}.svg', ContentFile(file_content), save=False)
+        self.drawing.save(f'{self.chain.id}_{self.chain.session.game_code}_{self.chain.session.round}.svg', ContentFile(file_content), save=False)
 
         # Save the instance again
         super().save(*args, **kwargs)
+    
+    def delete(self, *args, **kwargs):
+        # Delete the file
+        if os.path.exists(self.drawing.file.name):
+            os.remove(self.drawing.file.name)
+        
+        # Delete the instance
+        super().delete(*args, **kwargs)
 
     def __str__(self):
-        return self.id
+        return str(self.id)
