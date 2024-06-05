1e3794351f58fced77aa2a3c63ee7b863fa5f375
Alex Wu <alex.wu1288@gmail.com>
2024-06-05 00:53:43 -0700

    Reformat StartingPromptRound ui and remove migrations from repo

--- client/src/pages/StartingPromptRound.jsx
+++ client/src/pages/StartingPromptRound.jsx
@@ -2,16 +2,28 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import { TextInput, TopBar2 } from "../components";
 
-const StartingPromptRound = () => {
+const StartingPromptRound = (gameId) => {
     const navigate = useNavigate();
+    const [description, setDescription] = useState('');
+    const [isEditing, setIsEditing] = useState(true);
     const [countdown, setCountdown] = useState(5); // Initialize countdown (5 seconds)
+
+    console.log(gameId);
+
+    const handleInputChange = (e) => {
+        setDescription(e.target.value);
+    };
+
+    const handleButtonClick = () => {
+        setIsEditing(!isEditing);
+    };
 
     useEffect(() => {
         const timer = setInterval(() => {
             setCountdown((prevCountdown) => {
                 if (prevCountdown <= 1) {
                     clearInterval(timer); // Stop the interval when countdown is 0
-                    navigate('/drawing-round'); // Navigate when countdown is finished
+                    //navigate('/drawing-round'); // Navigate when countdown is finished
                     return 0;
                 }
                 return prevCountdown - 1; // Decrement the countdown by 1
@@ -36,47 +48,31 @@ const StartingPromptRound = () => {
 
 
     return (
-        <div className='w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgColor lg:rounded-lg h-screen overflow-hidden'>
+        <div className='w-full px-0 pb-20 2xl:px-40 bg-bgColor h-screen overflow-hidden flex flex-col justify-center items-center'>
             <TopBar2 />
-
-            <div className='w-full h-1/3 flex flex-row gap-2 mb-1 justify-center'>
-                <span className='text-2xl text-[#065ad8] font-semibold '>
-                    {countdown} second(s) left before round ends
-                </span>
+            <div className="w-full flex justify-center p-5 bg-[rgb(var(--color-grey))]">
+                <span className='colored-subtitle-text pr-2'>Type in a prompt:</span>
             </div>
 
-            <div className='w-full flex flex-row gap-2 mb-1 justify-center'>
-                <span className='text-2xl text-[#065ad8] font-semibold '>
-                    Type in a prompt
-                </span>
-            </div>
-
-            <form className='pb-20 flex flex-col gap-5' >
-                <div className='w-full flex flex-col lg:flex-row gap-1 md:gap-2'>
-                    <TextInput
-                        name='Prompt'
-                        placeholder='A dog is eating a banana.'
-                        label='Sentence'
-                        type='text'
-                        styles='w-full'
-                        disabled={isReady}
-                    />
-                </div>
-            </form>
-
-            <div>
-                <button
-                    className='w-full flex flex-col lg:flex-row gap-1 md:gap-2'
-                    onClick={toggleReady}
-                    style={{
-                        backgroundColor: isReady ? 'green' : 'red', // Conditional styling
-                        color: 'white'
-                    }}
-                >
-                    {isReady ? 'Ready' : 'Not Ready'}
+            <div className='flex items-center mb-[1%] mt-[5%] gap-3'>
+                <TextInput
+                    placeholder='...a dog eating a banana'
+                    type='text'
+                    value={description}
+                    styles="w-[400px] rounded-full"
+                    onChange={handleInputChange}
+                    disabled={!isEditing}/>
+                
+                <button className='colored-button-style mt-2.5' onClick={handleButtonClick}>
+                    {isEditing ? 'Ready!' : 'Not Ready'}
                 </button>
             </div>
 
+            <div className='w-full h-1/3 flex flex-row gap-2 mb-1 justify-center'>
+                <span className='text-normal text-ascent-1 '>
+                    {countdown} second(s) left before round ends
+                </span>
+            </div>
         </div>
     );
 }
