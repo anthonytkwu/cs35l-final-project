f4da5c9debc4666e6f3e0d5df1312c27cb0d30b0
Jacob Freund <jacobfreund01@gmail.com>
2024-06-06 04:59:03 -0700

    working aws branch

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,7 @@
 
 
-export const apiUrl = "http://127.0.0.1:8000";
+//export const apiUrl = "http://127.0.0.1:8000";
+export const apiUrl = "http://13.52.151.207:8000";
 
 export const ACCESS_TOKEN = "access";
 export const REFRESH_TOKEN = "refresh";
