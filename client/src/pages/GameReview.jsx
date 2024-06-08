614a5cc8f8bd0f1f64fb94fde1367cee3a00b4be
Alex Wu <alex.wu1288@gmail.com>
2024-06-07 18:56:55 -0700

    Can render game information now

--- client/src/pages/GameReview.jsx
+++ client/src/pages/GameReview.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { TopBar2, CustomButton, UserCard } from "../components";
-import { getGameInformation } from "../api";
+import { getGameInformation, interceptSVG } from "../api";
+import { intercept } from "../hooks/Intercept.js";
 import { useNavigate } from "react-router-dom";
 
 const GameReview = () => {
@@ -10,9 +11,8 @@ const GameReview = () => {
     const navigate = useNavigate();
 
     const [selectedPlayer, setSelectedPlayer] = useState(null);
-    const [userDescriptions, setUserDescriptions] = useState({});
-    const [userImages, setUserImages] = useState({});
-
+    const [userAlbum, setUserAlbum] = useState({});
+    const [displayCount, setDisplayCount] = useState(1);
     const PlayerList = ({ players, selectedPlayer, onSelectPlayer }) => {
         return (
             <div className='player-list-container-style bg-[rgb(var(--color-grey))]'>
@@ -28,9 +28,12 @@ const GameReview = () => {
         );
     };
 
-    function goToHome() {
-        navigate(`/home`);
-    }
+    const addUserAlbum = (username, album) =>{
+        setUserAlbum(prevState => ({
+            ...prevState,
+            [username]: album,
+        }));
+    };
 
     // Sample SVGs and strings
     const [svgs, setSvgs] = useState([
@@ -58,10 +61,11 @@ const GameReview = () => {
         if (i < svgs.length) combinedArray.push({ type: 'svg', content: svgs[i] });
     }
 
-    const [displayCount, setDisplayCount] = useState(0);
+    
 
     const handleForward = () => {
-        if (displayCount < combinedArray.length) {
+        if (selectedPlayer == null) return;
+        if (displayCount < userAlbum[selectedPlayer].length) {
             setDisplayCount(displayCount + 1);
         }
     };
@@ -72,8 +76,40 @@ const GameReview = () => {
         }
     };
 
+    function goToHome() {
+        navigate(`/home`);
+    }
+
     const handleSelectPlayer = (player) => {
         setSelectedPlayer(player);
+    };
+
+    async function getDescription(gameChain, roundNumber, navigate) {
+
+        const gameCode = localStorage.getItem('game_code');
+        const url = `/api/session/${gameCode}/${roundNumber}/${gameChain}/getDesc/`
+
+        try{
+            const data = await intercept(url, 'GET', null, navigate);
+            return data.description;
+        } catch(error){
+            console.error('Error occurred', error);
+                return null;
+        }
+    }
+
+    async function getImage (gameChain, roundNumber) {
+        const gameCode = localStorage.getItem('game_code');
+        const url = `/api/session/${gameCode}/${roundNumber}/${gameChain}/getDraw/`;
+        
+        try {
+            const response = await intercept(url, 'GET', null, navigate);
+            const svgResponse = await interceptSVG(`${response.drawing}/`, 'GET', null, navigate);
+            //setImg(`data:image/svg+xml;base64,${btoa(svgResponse)}`);  
+            return svgResponse;
+        } catch (error) {
+            console.error('Error occurred while fetching image:', error);
+        }
     };
 
     async function fetchData() {
@@ -81,18 +117,51 @@ const GameReview = () => {
             console.log("Fetching game information...");
             const data = await getGameInformation(localStorage.getItem('game_code'));
             console.log(data);
+
             setGameInfo(data); // Set gameInfo state variable with fetched data
             setPlayers(data.users);
+            console.log(data.users[0]);
+            setSelectedPlayer(data.users[0]);
+
             for (let i = 0;i<data.users.length;i++){
-                compileUserData(data.users[i]);
+                await compileUserData(data, data.users[i]);
             }
+
+            console.log("Selected player: " + selectedPlayer + " userAlbum " + userAlbum[selectedPlayer] + " isArray: " + Array.isArray(userAlbum[selectedPlayer]));
         } catch (error) {
             setErrMsg({ message: error.message, status: 'failed' });
         }
     }
 
-    async function compileUserData(user){
-        console.log(user);
+    function getUserChain(gameData, user){
+        const keys = Object.keys(gameData.chains);
+
+        for (let i = 0;i < keys.length; i++){
+            if (gameData.chains[keys[i]][0] == user){
+                return keys[i];
+            }
+        }
+    }
+
+    async function compileUserData(gameData, user){
+        const userChain = getUserChain(gameData, user);
+        let localAlbum = [];
+
+        if (userChain == undefined){
+            throw new Error("Could not find user chain value");
+        }else{
+            for (let round = 0; round < gameData.users.length; round++){
+                if (round % 2 == 0){
+                    let descResponse = await getDescription(userChain, round, navigate);
+                    localAlbum.push({ type: 'string', content: descResponse });
+                }else{
+                    let svgResponse = await getImage(userChain, round);
+                    
+                    localAlbum.push({ type: 'svg', content: `data:image/svg+xml;base64,${btoa(svgResponse)}` });
+                }
+            }
+        }
+        addUserAlbum(user, localAlbum);
     }
 
     useEffect(() => {
@@ -100,18 +169,44 @@ const GameReview = () => {
     }, []);
 
     return (
-        <div className="flex flex-col justify-start h-screen bg-bgColor">
+        <div className="flex flex-col justify-start bg-bgColor">
             <div><TopBar2 /></div>
             <div className='flex flex-row gap-[10px] justify-center m-[2%]'>
-                <div className='w-1/5 h-[75vh] rounded-[5vh] bg-[rgb(var(--color-grey))]'>
+                <div className='w-1/5 h-[75vh] rounded-[5vh] bg-[rgb(var(--color-grey))] cursor-pointer'>
                     <PlayerList 
                         players={players} 
                         selectedPlayer={selectedPlayer} 
                         onSelectPlayer={handleSelectPlayer} />
                 </div>
                 <div className='w-2/5 h-[75vh] rounded-[5vh] bg-[rgb(var(--color-grey))]'>
-                    <div>{selectedPlayer}</div>
+                    {selectedPlayer && userAlbum[selectedPlayer] && Array.isArray(userAlbum[selectedPlayer]) ? (
+                        userAlbum[selectedPlayer].slice(0, displayCount).map((item, index) => (
+                            <div key={index}>
+                                {item && item.type === 'string' ? (
+                                    <p>{item.content}</p>
+                                ) : item && item.type === 'svg' ? (
+                                    <img src={item.content} alt="drawing" />
+                                ) : null}
+                            </div>
+                        ))
+                    ) : (
+                        <div className="mt-5 ml-5 text-[rgb(var(--color-ascent1))]">No data available</div>
+                    )}
                 </div>
+            </div>
+            <div className="flex flex-row justify-center gap-[10px] pb-4">
+                <CustomButton
+                        onClick={goToHome}
+                        containerStyles={'colored-button-style w-[170px]'}
+                        title='Back to home' />
+                <CustomButton
+                    onClick={handleBackward}
+                    containerStyles={'colored-button-style w-[170px]'}
+                    title='Previous' />
+                <CustomButton
+                    onClick={handleForward}
+                    containerStyles={'colored-button-style w-[170px]'}
+                    title='Next' />
             </div>
 
             {/* <div className='flex flex-col gap-[10px] overflow-y-scroll justify-center m-[2%] items-center'>
@@ -129,21 +224,6 @@ const GameReview = () => {
                         </div>
                     ))}
                 </div>
-                <div className='flex flex-row gap-10'>
-                    <CustomButton
-                        onClick={handleBackward}
-                        containerStyles={'colored-button-style w-24 h-12'}
-                        title='Previous' />
-                    <CustomButton
-                        onClick={handleForward}
-                        containerStyles={'colored-button-style w-24 h-12'}
-                        title='Next' />
-
-                </div>
-                <CustomButton
-                        onClick={goToHome}
-                        containerStyles={'colored-button-style'}
-                        title='Back to home' />
             </div> */}
         </div>
     );
