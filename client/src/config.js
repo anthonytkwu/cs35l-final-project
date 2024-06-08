74b96f2b670093aa204ac5467d310f847a0498ef
anthonytkwu <anthonytkwu@gmail.com>
2024-06-07 21:16:20 -0700

    changes to run on lan

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 
 
-export const apiUrl = "ec2-50-18-247-211.us-west-1.compute.amazonaws.com:8000";
+export const apiUrl = "http://192.168.7.175:8000";
 
 export const ACCESS_TOKEN = "access";
 export const REFRESH_TOKEN = "refresh";
